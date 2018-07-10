be85a8935f6c2e8a2c68b4a4197e7012e6c05628
Oyinda David <oyetunmibidavid9@gmail.com>
2018-07-10 13:59:36 +0000

    Textarea and Element Component

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -2,46 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class Dashboard extends React.Component{
-  constructor(props){
-    super(props)
-  }
 
-  render(){
-    return(
-      <div>
-        Markdown Previewer Dashboard
-        </div>
+
+class TextArea extends Component {
+  render() {
+    return (
+      <div style={{marginTop : 50}}>
+        <textarea id="editor"></textarea>
+      </div>
     );
   }
 }
 
-class Previewer extends React.Component{
-  constructor(props){
-    super(props)
-  }
-
+class Element extends Component {
   render(){
     return(
       <div>
-        Markdown Previewer Dashboard
+        <p>
+         Me and Her!
+        </p>
+
         </div>
     );
   }
 }
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <TextArea />
+      <Element />
       </div>
     );
   }
